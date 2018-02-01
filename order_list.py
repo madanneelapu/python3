628778d5e71537a9669e73e29c0b0ab099564be7
Madan <madanneelapu@gmail.com>
2018-02-01 14:04:16 +0530

    sorting logic based on lambda

--- order_list.py
+++ order_list.py
@@ -1,13 +1,14 @@
 def order_my_list(values,order="n"):
 
-    def isorder():
-        return order == "r"
+    #def isorder():
+    #    return order == "r"
+      
+    order_lambda = lambda o: o == "r"
 
-    for v in sorted(values, reverse=1):
+    for v in sorted(values,reverse=order_lambda(order)):
         print(v)
 
 
 nums = [12,25,65,14,36,98]
-
-order_my_list(nums)
+order_my_list(nums,"r")
 
