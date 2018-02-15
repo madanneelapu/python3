ea0badf4c70cc6855e059eb80bca63fd948931b9
Madan Neelapu <madanneelapu@gmail.com>
2018-02-15 18:39:47 +0530

    source code listing program

--- file_handling/source_code_listing.py
+++ file_handling/source_code_listing.py
@@ -1,2 +1,25 @@
 # Take a path and print all '.py' files with name, content along with line numbers
 # filter files that contain only 'file' in filename
+import os
+
+base_dir = "F:\\DevEnv\\workspace\\PyCharmProjects\\python3\\file_handling"
+pattern = "emp"
+files = os.listdir(base_dir)
+try:
+    for file in files:
+        if file.endswith(".py") and file.find(pattern) != -1:
+            file_full_path = os.path.join(base_dir,file)
+            print(file_full_path)
+            print("-" * len(file_full_path))
+            with open(file_full_path) as f:
+                lines = f.readlines()
+                linenum = 1
+                for line in lines:
+                    print( "{0:3d} {1}".format(linenum, line), end="")
+                    linenum += 1
+            print()
+            print("=" * len(file_full_path))
+
+
+except Exception as ex:
+    print("Error: ",ex)
