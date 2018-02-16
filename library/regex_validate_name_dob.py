06101792153a475f555d736e29f3db7918b6273f
Madan Neelapu <madanneelapu@gmail.com>
2018-02-16 18:18:45 +0530

    Reg Ex Examples with File API

--- library/regex_validate_name_dob.py
+++ library/regex_validate_name_dob.py
@@ -1,1 +1,22 @@
 # validate name(incl spaces and dots) and DOB(yyyy-mm-dd,yyyy-m-d) using regular expression.
+
+import re
+
+valid_recs = []
+try:
+    with open("names_dobs.txt", "r") as f:
+        lines = f.readlines();
+        for line in lines:
+            line = line.strip("\n")
+            print(line)
+            m1 = re.match("^[a-zA-Z]*\.?\s*[a-zA-Z]*\s*(\d{1,4}-\d{1,2}-\d{1,2})$", line)
+            if (m1):
+                print("Matched: ", line)
+                print("dob:",m1.group(1))
+                valid_recs.append(line)
+
+    print(valid_recs)
+
+
+except Exception as ex:
+    print("error: ", ex)
--- library/regex_vehicle_nums.py
+++ library/regex_vehicle_nums.py
@@ -1,2 +1,29 @@
 # Read a file with vehicle numbers and print valid vehicle numbers with regular expression
 # sort them by serial number
+import re
+
+
+def extract_sl_no(vnum):
+    m2 = re.search("^[a-zA-Z]{2}\s*\d{1,2}\s*[a-zA-Z]{1,2}\s*(\d{1,4})$", vnum)
+    return int(m2.group(1))
+
+
+valid_nums = set()
+try:
+    with open("vehicle_nums.txt", "r") as f:
+        lines = f.readlines();
+        for line in lines:
+            line = line.strip("\n")
+            # print(line)
+            m1 = re.match("^[a-zA-Z]{2}\s*\d{1,2}\s*[a-zA-Z]{1,2}\s*\d{1,4}$", line)
+            if (m1):
+                print("Matched: ", line)
+                valid_nums.add(line)
+
+    print(valid_nums)
+
+    for num in sorted(valid_nums, key=lambda l: extract_sl_no(l)):
+        print(num)
+
+except Exception as ex:
+    print("error: ", ex)
